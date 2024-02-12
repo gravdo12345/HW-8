7f378097a7353c213005891db68a581566ee8f17
GRAVDO <gravdo.d@gmail.com>
2024-02-12 01:16:07 +0100

    commit

--- src/main/java/org/example/DatabaseQueryService.java
+++ src/main/java/org/example/DatabaseQueryService.java
@@ -1,5 +1,4 @@
 package org.example;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,54 +16,197 @@ public class DatabaseQueryService {
         this.connection = connection;
     }
 
-    public List<ProjectInfo> findLongestProject() {
-        return executeQuery("find_longest_project.sql");
+    // робота PreparedStatement
+    private ResultSet executePreparedStatement(String sqlFilePath, String... params) throws IOException {
+        String sqlQuery = readSqlFile(sqlFilePath);
+        try {
+            PreparedStatement statement = connection.prepareStatement(sqlQuery);
+            for (int i = 0; i < params.length; i++) {
+                statement.setString(i + 1, params[i]);
+            }
+            return statement.executeQuery();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IOException("Failed to execute prepared statement", e);
+        }
     }
 
-    private List<ProjectInfo> executeQuery(String sqlFilePath) {
-        List<ProjectInfo> results = new ArrayList<>();
-
+    // читає файли(букварик містний)
+    private String readSqlFile(String sqlFilePath) throws IOException {
         try {
-            String sqlQuery = readSqlFile(sqlFilePath);
+            return Files.readString(Path.of(sqlFilePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IOException("Failed to read SQL file", e);
+        }
+    }
 
-            try (PreparedStatement statement = connection.prepareStatement(sqlQuery)) {
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    while (resultSet.next()) {
-                        String projectName = resultSet.getString("PROJECT_NAME");
-                        int monthCount = resultSet.getInt("MONTH_COUNT");
 
-                        ProjectInfo projectInfo = new ProjectInfo(projectName, monthCount);
-                        results.add(projectInfo);
-                    }
-                }
+    public List<ProjectInfo> findLongestProject() {
+        List<ProjectInfo> projects = new ArrayList<>();
+        try {
+            ResultSet resultSet = executePreparedStatement("find_longest_project.sql");
+            while (resultSet.next()) {
+                int projectId = resultSet.getInt("PROJECT_ID");
+                int durationInDays = resultSet.getInt("DURATION_IN_DAYS");
+                projects.add(new ProjectInfo(projectId, durationInDays));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return results;
+        return projects;
     }
 
-    // це для читання(букварик типу, ну, жарт короче)
-    private String readSqlFile(String sqlFilePath) throws IOException {
-        return Files.readString(Path.of(sqlFilePath));
+
+    public List<ClientInfo> findMaxProjectsClient() {
+        List<ClientInfo> clients = new ArrayList<>();
+        try {
+            ResultSet resultSet = executePreparedStatement("find_max_projects_client.sql");
+            while (resultSet.next()) {
+                String clientName = resultSet.getString("NAME");
+                int projectCount = resultSet.getInt("PROJECT_COUNT");
+                clients.add(new ClientInfo(clientName, projectCount));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return clients;
     }
 
-    // передає інформацію(через границю)
+
+    public List<WorkerInfo> findMaxSalaryWorker() {
+        List<WorkerInfo> workers = new ArrayList<>();
+        try {
+            ResultSet resultSet = executePreparedStatement("find_max_salary_worker.sql");
+            while (resultSet.next()) {
+                String workerName = resultSet.getString("NAME");
+                int salary = resultSet.getInt("SALARY");
+                workers.add(new WorkerInfo(workerName, salary));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return workers;
+    }
+
+
+    public List<WorkerInfo> findYoungestAndEldestWorkers() {
+        List<WorkerInfo> workers = new ArrayList<>();
+        try {
+            ResultSet resultSet = executePreparedStatement("find_youngest_eldest_workers.sql");
+            while (resultSet.next()) {
+                String workerName = resultSet.getString("NAME");
+                String workerType = resultSet.getString("TYPE");
+                String birthday = resultSet.getString("BIRTHDAY");
+                workers.add(new WorkerInfo(workerName, workerType, birthday));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return workers;
+    }
+
+
+    public List<ProjectPrice> printProjectPrices() {
+        List<ProjectPrice> prices = new ArrayList<>();
+        try {
+            ResultSet resultSet = executePreparedStatement("print_project_prices.sql");
+            while (resultSet.next()) {
+                int projectId = resultSet.getInt("PROJECT_ID");
+                int price = resultSet.getInt("PRICE");
+                prices.add(new ProjectPrice(projectId, price));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return prices;
+    }
+
+
     public static class ProjectInfo {
-        private final String projectName;
-        private final int monthCount;
+        private final int projectId;
+        private final int durationInDays;
 
-        public ProjectInfo(String projectName, int monthCount) {
-            this.projectName = projectName;
-            this.monthCount = monthCount;
+        public ProjectInfo(int projectId, int durationInDays) {
+            this.projectId = projectId;
+            this.durationInDays = durationInDays;
         }
 
-        public String getProjectName() {
-            return projectName;
+        public int getProjectId() {
+            return projectId;
         }
 
-        public int getMonthCount() {
-            return monthCount;
+        public int getDurationInDays() {
+            return durationInDays;
+        }
+    }
+
+
+    public static class ClientInfo {
+        private final String clientName;
+        private final int projectCount;
+
+        public ClientInfo(String clientName, int projectCount) {
+            this.clientName = clientName;
+            this.projectCount = projectCount;
+        }
+
+        public String getClientName() {
+            return clientName;
+        }
+
+        public int getProjectCount() {
+            return projectCount;
+        }
+    }
+
+
+    public static class WorkerInfo {
+        private final String workerName;
+        private final String workerType;
+        private final String birthday;
+
+        public WorkerInfo(String workerName, int salary) {
+            this.workerName = workerName;
+            this.workerType = "Worker with highest salary";
+            this.birthday = "";
+        }
+
+        public WorkerInfo(String workerName, String birthday, String workerType) {
+            this.workerName = workerName;
+            this.workerType = workerType;
+            this.birthday = birthday;
+        }
+
+        public String getWorkerName() {
+            return workerName;
+        }
+
+        public String getWorkerType() {
+            return workerType;
+        }
+
+        public String getBirthday() {
+            return birthday;
+        }
+    }
+
+
+    public static class ProjectPrice {
+        private final int projectId;
+        private final int price;
+
+        public ProjectPrice(int projectId, int price) {
+            this.projectId = projectId;
+            this.price = price;
+        }
+
+        public int getProjectId() {
+            return projectId;
+        }
+
+        public int getPrice() {
+            return price;
         }
     }
 }
