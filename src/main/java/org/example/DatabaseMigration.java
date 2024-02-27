19d75b206813735975b1f4ff6e75aa7209b7d5a9
GRAVDO <42653608+gravdo12345@users.noreply.github.com>
2024-02-27 22:00:49 +0100

    Update DatabaseMigration.java

--- src/main/java/org/example/DatabaseMigration.java
+++ src/main/java/org/example/DatabaseMigration.java
@@ -8,7 +8,7 @@ public class DatabaseMigration {
         // Create Flyway instance
         Flyway flyway = Flyway.configure()
                 .dataSource("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "")
-                .locations("classpath:db/migration") //ya'll can modify this shi if ya have diferent directory
+                .locations("classpath:db/migration")//ya'll can modify this shi if ya have diferent directory
                 .load();
 
         flyway.migrate(); //lezzzzgooooo
